7c00ed9527e0c8a3d482bfe545294cdb83b42562
Nyolczas <nyolczas@gmail.com>
2018-04-20 19:53:01 +0200

    eseménykezelés

---
+++ VBProg-Youtube/Csere/Csere/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
+        PictureBox3.Image = PictureBox1.Image
+        PictureBox1.Image = PictureBox2.Image
+        PictureBox2.Image = PictureBox3.Image
+
+    End Sub
+
+    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles PictureBox2.Click
+        PictureBox3.Image = PictureBox1.Image
+        PictureBox1.Image = PictureBox2.Image
+        PictureBox2.Image = PictureBox3.Image
+    End Sub
+End Class
---
+++ VBProg-Youtube/EltűnőKép/EltűnőKép/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_MouseLeave(sender As Object, e As EventArgs) Handles MyBase.MouseLeave
+        PictureBox1.Visible = False
+    End Sub
+
+    Private Sub Form1_MouseEnter(sender As Object, e As EventArgs) Handles MyBase.MouseEnter
+        PictureBox1.Visible = True
+    End Sub
+End Class
