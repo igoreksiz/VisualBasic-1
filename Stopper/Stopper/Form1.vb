16dbb7da6a686c7a55cafc8d9bf6c7fc2c39126f
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-04-21 13:37:12 +0200

    Stopperóra

---
+++ Stopper/Stopper/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+    Dim EzredMásodperc, Másodperc, Perc, Óra As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        EzredMásodperc = 0
+        Másodperc = 0
+        Perc = 0
+        Óra = 0
+        Timer1.Interval = 10
+        Timer1.Start()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Timer1.Stop()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        EzredMásodperc += 1
+
+        If EzredMásodperc = 60 Then
+            Másodperc += 1
+            EzredMásodperc = 0
+        End If
+
+        If Másodperc = 60 Then
+            Perc += 1
+            Másodperc = 0
+        End If
+
+        If Perc = 60 Then
+            Óra += 1
+            Perc = 0
+        End If
+
+        Label1.Text = Óra & ":" & Format(Perc, "00") & ":" & Format(Másodperc, "00") & ":" & EzredMásodperc
+
+    End Sub
+End Class
---
+++ Stopper/Stopper/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Stopper")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("Stopper")>
+<Assembly: AssemblyCopyright("Copyright ©  2018")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("9602c2a7-067e-4a07-b0ae-e1cd5574f7a9")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
