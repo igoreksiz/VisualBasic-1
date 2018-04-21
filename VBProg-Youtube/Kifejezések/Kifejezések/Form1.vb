86f750380c692160aa20f3d57f914ae5aadfc0ff
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-04-21 09:28:02 +0200

    kifejezések

---
+++ VBProg-Youtube/Kifejezések/Kifejezések/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim a, c, m, t As Single
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        a = 3
+        c = 1
+        m = 2
+        t = (a + c) / 2 * m
+        Label1.Text = t
+
+        a = 6 * (16 - 4 * 3) / (2 + 24 / (5 - 2))
+        Label1.Text &= vbNewLine & a
+    End Sub
+End Class
