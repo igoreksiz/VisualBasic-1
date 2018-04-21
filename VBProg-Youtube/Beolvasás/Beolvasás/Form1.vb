65762d9321e2a0a342bc2dd9a943fc12b73a545f
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-04-21 10:02:01 +0200

    beolvasás

---
+++ VBProg-Youtube/Beolvasás/Beolvasás/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Dim Szám1, Szám2, Összeg As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Szám1 = Convert.ToInt32(TextBox1.Text)
+        Szám2 = Convert.ToInt32(TextBox2.Text)
+        Összeg = Szám1 + Szám2
+        Label1.Text = Összeg
+
+    End Sub
+End Class
