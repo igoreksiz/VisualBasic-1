9cfbf3729f685e4d44e76200b77e97a60074e5bb
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-04-21 12:45:40 +0200

    képméretező

---
+++ EgyÁgú/EgyÁgú/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+
+    Dim Szám As Single
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Szám = TextBox1.Text
+        Label1.Text = "A " & Szám & vbNewLine
+        Label1.Text &= "Abszolút értéke: "
+        Label1.Text &= Math.Abs(Szám) & vbNewLine
+
+        If Szám <> 0 Then
+            Label1.Text &= "Reciproka: "
+            Label1.Text &= 1 / Szám & vbNewLine
+        End If
+
+        If Szám >= 0 Then
+            Label1.Text &= "Négyzetgyöke: "
+            Label1.Text &= Math.Sqrt(Szám)
+        End If
+
+    End Sub
+End Class
---
+++ Méretez/Méretez/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        If PictureBox1.Width > 10 Then
+            PictureBox1.Width = PictureBox1.Width / 1.2
+            PictureBox1.Height = PictureBox1.Height / 1.2
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        If PictureBox1.Width < 1000 Then
+            PictureBox1.Width = PictureBox1.Width * 1.2
+            PictureBox1.Height = PictureBox1.Height * 1.2
+        End If
+
+    End Sub
+End Class
