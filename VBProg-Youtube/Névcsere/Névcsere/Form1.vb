462c96d8f69be751a5e8f61d017e29f97fea26b8
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-04-21 09:11:42 +0200

    névcsere

---
+++ VBProg-Youtube/Névcsere/Névcsere/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Dim Név1, Név2, Temp As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Név1 = TextBox1.Text
+        Név2 = TextBox2.Text
+
+        Temp = Név1
+
+        Név1 = Név2
+        Név2 = Temp
+
+        TextBox1.Text = Név1
+        TextBox2.Text = Név2
+
+    End Sub
+End Class
