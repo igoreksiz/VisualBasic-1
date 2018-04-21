9e022b8d02e34b7e64404b47d4bfe93013bc40d2
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-04-21 12:15:48 +0200

    szelekció

---
+++ VBProg-Youtube/Szelekció/Szelekció/Form1.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form1
+    Dim Osztandó, Osztó, Hányados As Single
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Osztandó = TextBox1.Text
+        Osztó = TextBox2.Text
+
+        If Osztó = 0 Then
+            Label1.Text = "Nullával nem lehet osztani!"
+        Else
+            Hányados = Osztandó / Osztó
+            Label1.Text = Hányados
+        End If
+        Label1.Text &= vbNewLine
+
+    End Sub
+End Class
