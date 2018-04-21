06960855c6891f39273b71ee29db780b4621a939
Nyolczas <nyolczas@gmail.com>
2018-04-21 22:06:40 +0200

    FájlBe

--- VBProg-Youtube/FájlbólOlvas/FájlbólOlvas/Form1.vb
+++ VBProg-Youtube/FájlbólOlvas/FájlbólOlvas/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FájlBe As New IO.StreamReader("C:\Users\User\Documents\Tozsde\MT4ek\XM1\MQL4\Files\webStatement\data\EURJPY\EURJPY_M15_TSQ.csv")
 
+        Dim Sor As String
+        ' Előolvasás
+        Sor = FájlBe.ReadLine()
+        Do While Sor IsNot Nothing
+            Label1.Text &= Sor & vbNewLine ' már beolvastuk a sort
+            Sor = FájlBe.ReadLine()  ' a végén Nothing lesz
+        Loop
+
+        FájlBe.Close()
+        Label1.Text &= vbNewLine & vbNewLine & "A Beolvasás kész." & vbNewLine & vbNewLine
+
+
+
+    End Sub
 End Class
