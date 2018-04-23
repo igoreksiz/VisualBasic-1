43f06f1ddee4c864f5783d9bb49128b1d8e629c5
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-04-23 17:48:24 +0200

    naplózó 2

---
+++ Naplózó/Naplózó2/Form1.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Dátum = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.érték = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Label1 = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Dátum, Me.érték})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 41)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Dátum
+        '
+        DataGridViewCellStyle3.Format = "d"
+        DataGridViewCellStyle3.NullValue = Nothing
+        Me.Dátum.DefaultCellStyle = DataGridViewCellStyle3
+        Me.Dátum.Frozen = True
+        Me.Dátum.HeaderText = "Dátum"
+        Me.Dátum.Name = "Dátum"
+        '
+        'érték
+        '
+        DataGridViewCellStyle4.Format = "N0"
+        DataGridViewCellStyle4.NullValue = Nothing
+        Me.érték.DefaultCellStyle = DataGridViewCellStyle4
+        Me.érték.Frozen = True
+        Me.érték.HeaderText = "érték"
+        Me.érték.Name = "érték"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(291, 41)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 13)
+        Me.Label1.TabIndex = 2
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Dátum As DataGridViewTextBoxColumn
+    Friend WithEvents érték As DataGridViewTextBoxColumn
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Naplózó/Naplózó2/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Structure TSor
+        Dim Dátum As Date
+        Dim Egyenleg As Single
+    End Structure
+
+    Dim Sorok(9999999) As TSor
+    Dim SorCount As Integer
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FájlBe As New IO.StreamReader("naplo.csv")
+
+        ' beolvasás
+        Dim Temp() As String
+        SorCount = 0
+        Do While FájlBe.Peek <> -1
+            Temp = FájlBe.ReadLine.Split(";")
+            SorCount += 1
+            With Sorok(SorCount)
+                .Dátum = Temp(0)
+                .Egyenleg = Temp(1)
+                Label1.Text += "dátum " & SorCount & " " & .Dátum & vbNewLine
+            End With
+        Loop
+
+        ' táblázat feltöltése
+        FájlBe.Close()
+        For J = 1 To SorCount
+
+            DataGridView1.Rows.Add("")
+            With Sorok(J)
+
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(0).Value = .Dátum
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(1).Value = .Egyenleg
+                Label1.Text += DataGridView1.Rows.Count - 1 & vbNewLine
+            End With
+        Next
+
+    End Sub
+End Class
