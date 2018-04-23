b89fc6f152ca0c53d242d5c6885574e81fd484a3
Istvan Nyolczas <31048051+Nyolczas@users.noreply.github.com>
2018-04-23 12:49:24 +0200

    Naplózó beolvasás

--- Naplózó/Naplózó/Form1.Designer.vb
+++ Naplózó/Naplózó/Form1.Designer.vb
@@ -22,10 +22,67 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim DataGridViewCellStyle1 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Dátum = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.érték = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Dátum, Me.érték})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 41)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 150)
+        Me.DataGridView1.TabIndex = 1
+        '
+        'Dátum
+        '
+        DataGridViewCellStyle1.Format = "d"
+        DataGridViewCellStyle1.NullValue = Nothing
+        Me.Dátum.DefaultCellStyle = DataGridViewCellStyle1
+        Me.Dátum.Frozen = True
+        Me.Dátum.HeaderText = "Dátum"
+        Me.Dátum.Name = "Dátum"
+        '
+        'érték
+        '
+        DataGridViewCellStyle2.Format = "N0"
+        DataGridViewCellStyle2.NullValue = Nothing
+        Me.érték.DefaultCellStyle = DataGridViewCellStyle2
+        Me.érték.Frozen = True
+        Me.érték.HeaderText = "érték"
+        Me.érték.Name = "érték"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Dátum As DataGridViewTextBoxColumn
+    Friend WithEvents érték As DataGridViewTextBoxColumn
 End Class
--- Naplózó/Naplózó/Form1.vb
+++ Naplózó/Naplózó/Form1.vb
@@ -1,3 +1,19 @@
 ﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim FájlBe As New IO.StreamReader("naplo.csv")
+        Dim Sor As String
+        Dim Temp() As String
 
+        Do
+            Sor = FájlBe.ReadLine
+            If Sor Is Nothing Then Exit Do
+            Temp = Sor.Split(";")
+            DataGridView1.Rows.Add("")
+            For i As Integer = 0 To 1
+                DataGridView1.Rows(DataGridView1.Rows.Count - 1).Cells(i).Value = Temp(i)
+            Next
+        Loop
+
+        FájlBe.Close()
+    End Sub
 End Class
