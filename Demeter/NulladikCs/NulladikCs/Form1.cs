a0df9bb8c390ef141d455b9a3a819a61b2eca500
Nyolczas <nyolczas@gmail.com>
2018-05-15 18:52:36 +0200

    nulladik cs

---
+++ Demeter/NulladikCs/NulladikCs/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NulladikCs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (button1.Text=="Elrejt")
+            {
+                label1.Hide();
+                button1.Text = "Megmutat";
+            }
+            else
+            {
+                label1.Show();
+                button1.Text = "Elrejt";
+            }
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            Int32 HolX;
+            Int32 HolY;
+
+            HolX = (this.Width - label1.Width) / 2;
+            HolY = (this.ClientSize.Height - label1.Height) / 2;
+
+            label1.SetBounds(HolX, HolY, label1.Width, label1.Height);
+        }
+    }
+}
