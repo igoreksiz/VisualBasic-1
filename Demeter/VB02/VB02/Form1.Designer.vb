128bf6dfc20e552200c02c33e7aa570d7060e995
Nyolczas <nyolczas@gmail.com>
2018-05-17 21:49:15 +0200

    vb2

--- Demeter/VB02/VB02/Form1.Designer.vb
+++ Demeter/VB02/VB02/Form1.Designer.vb
@@ -22,10 +22,19 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.VB02.My.Resources.Resources.abstract
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
--- Demeter/VB02/VB02/Form1.vb
+++ Demeter/VB02/VB02/Form1.vb
@@ -1,3 +1,31 @@
 ﻿Public Class Form1
-
+    Private Sub Form1_MouseClick(sender As Object, e As MouseEventArgs) Handles Me.MouseClick
+        Dim MiniInfo As New ToolTip
+        Dim mit As String
+        MiniInfo.AutoPopDelay = 1000
+        MiniInfo.InitialDelay = 100
+        MiniInfo.ReshowDelay = 500
+        MiniInfo.ShowAlways = True
+        If e.Location.X < sender.Width / 2 And e.Location.Y < sender.ClientSize.Height / 2 Then
+            If e.Button = Windows.Forms.MouseButtons.Left Then
+                mit = " Ablakkeret stílusa "
+                If Me.FormBorderStyle < 6 Then
+                    FormBorderStyle = FormBorderStyle + 1
+                ElseIf CInt(Me.FormBorderStyle) = 6 Then
+                    FormBorderStyle = 0
+                End If
+            ElseIf e.Button = Windows.Forms.MouseButtons.Right Then
+                mit = " Control menü "
+                Me.ControlBox = Not Me.ControlBox
+            End If
+        ElseIf e.Location.X > Me.Width / 2 And e.Location.Y < Me.ClientSize.Height / 2 Then
+            If e.Button = Windows.Forms.MouseButtons.Left Then
+                mit = " Ablak átlátszóságának növelése "
+                If Me.Opacity > 0.2 Then
+                    Me.Opacity -= 0.1
+                End If
+            ElseIf e.Button = Windows.Forms.MouseButtons.Right Then
+            End If
+        End If
+    End Sub
 End Class
